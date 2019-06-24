c24b10289cc2a0f31f6bbe22a9360191701269b7
Bastian Wagner <bastian.wagner@softconcis.de>
2019-06-24 16:46:24 +0200

    manifest: android:theme von GdxTheme auf @style/Theme.AppCompat geaendert

--- android/src/com/dandelionrace/game/MainActivity.kt
+++ android/src/com/dandelionrace/game/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /* println("Start"); */
         setContentView(R.layout.activity_main)
     }
 }
