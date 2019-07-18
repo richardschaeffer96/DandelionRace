5c15bbb284d59bfcfae1c6462bc0527afcada98c
Felix Decher <decherf@cip.ifi.lmu.de>
2019-07-18 20:34:53 +0200

    New GameOverScreen with Button to get Back to the Main Activity

--- android/src/com/dandelionrace/game/States/WinGame.kt
+++ android/src/com/dandelionrace/game/States/WinGame.kt
@@ -8,17 +8,38 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.dandelionrace.game.dandelionrace
 import com.dandelionrace.game.lobby.EntryHallActivity
 import com.dandelionrace.game.sprites.Bird
+import android.support.v4.content.ContextCompat.startActivity
+import android.widget.ImageView
+import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter
+import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.dandelionrace.game.LoginActivity
+import com.dandelionrace.game.MainActivity
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import android.R.attr.button
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import android.R.attr.button
+import android.util.EventLog
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import java.util.*
+
 
 class WinGame(gsm: GameStateManager, winner: Boolean) : State(gsm) {
 
     private val win: Texture
     val app_width: Float
     val app_height: Float
-    private val playBtn: Texture
+    private val playButton: ImageButton
+    private val stage: Stage
 
     private val bird: Bird
 
     init {
+
         if (winner){
             win = Texture("winscreen.jpg")
         }else{
@@ -27,14 +48,20 @@ class WinGame(gsm: GameStateManager, winner: Boolean) : State(gsm) {
         bird = Bird(100,500, 0)
         app_height = Gdx.app.graphics.height.toFloat()
         app_width = Gdx.app.graphics.width.toFloat()
-        playBtn = Texture("playBtn.png")
+
+        stage = Stage()
+        Gdx.input.inputProcessor = stage
+        val drawable = TextureRegionDrawable (TextureRegion(Texture("playBtn.png")))
+        playButton = ImageButton(drawable);
+        stage.addActor(playButton);
+
+        playButton.addListener(object : ClickListener() {
+            override  fun clicked(event: InputEvent, x: Float, y: Float) {
+                Gdx.app.exit()
+            }
+        })
     }
     override fun update(dt: Float) {
-        bird.update(dt)
-
-        cam.position.set(bird.position.x + 80, cam.viewportHeight/2,0f)
-        cam.update()
-        handleInput()
 
     }
 
@@ -43,24 +70,22 @@ class WinGame(gsm: GameStateManager, winner: Boolean) : State(gsm) {
         sb.projectionMatrix.set(cam.combined)
         sb.begin()
         sb.draw(win, cam.position.x - (cam.viewportWidth / 2 ), 0f, dandelionrace.WIDTH.toFloat(), dandelionrace.HEIGHT.toFloat())
-        sb.draw(playBtn, cam.position.x - (cam.viewportWidth / 4 - playBtn.width / 2).toFloat(), (app_height / 2).toFloat())
         sb.end()
+        playButton.x = cam.position.x - ((cam.viewportWidth / 2) - playButton.width)
+        playButton.y = (app_height / 2) -400
 
-
-
+        stage.draw()
 
     }
 
     override fun dispose() {
         win.dispose()
-        playBtn.dispose()
     }
 
 
     override fun handleInput() {
         if (Gdx.input.justTouched()) {
-            //gsm.set(MenuState(gsm))
-            dispose();
+           //dispose();
         }
     }
 
